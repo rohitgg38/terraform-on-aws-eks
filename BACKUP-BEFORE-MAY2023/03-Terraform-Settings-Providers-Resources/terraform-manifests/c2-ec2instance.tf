8699f6034fa4a093f37d02f0137504f473422e0a
rohit <rohitgg38@gmail.com>
2025-09-10 19:44:13 +0530

    AMID updated

--- BACKUP-BEFORE-MAY2023/03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ BACKUP-BEFORE-MAY2023/03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = "ami-00ca32bbc84273381"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
